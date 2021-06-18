036cecbffac258b27c3720869e97b245bc0347ac
Alemarnery <aleemar.95@gmail.com>
2021-06-17 21:45:38 -0500

    alignItems

--- layout.js
+++ layout.js
@@ -1,17 +1,48 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 const layout = () => (
   <SafeAreaView style={Style.SafeAreaViewStyles}>
-    <Text> Este es un texto de prueba</Text>
+    <View style={Style.ViewGreen} />
+    <View style={Style.ViewBlue} />
+    <View style={Style.ViewYellow} />
   </SafeAreaView>
 );
 
+//Justify Content y align items, se utilizan cuando los elementos tienen un HEIGHT determinado
+
+//JuSTIFY Content trabaja en fucion del eje princial, es decir, el vertical
+
 const Style = StyleSheet.create({
   SafeAreaViewStyles: {
-    borderWidth: 3,
+    borderWidth: 1,
     borderColor: "red",
+    flex: 1,
+    // flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  ViewGreen: {
+    borderWidth: 3,
+    borderColor: "green",
+    height: 60,
+    width: 30,
+    //flex: 1 / 2,
+  },
+  ViewBlue: {
+    borderWidth: 3,
+    borderColor: "blue",
+    height: 60,
+    width: 30,
+    //flex: 3 / 8,
+  },
+  ViewYellow: {
+    borderWidth: 3,
+    borderColor: "yellow",
+    height: 60,
+    width: 30,
+    // flex: 1 / 8,
   },
 });
 
